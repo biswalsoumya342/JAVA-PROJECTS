ab1da84fea8418c4ccc0a74e047116149f3615d7
Soamyaranjan Biswal <125767365+biswalsoumya342@users.noreply.github.com>
2025-01-03 12:33:35 +0530

    Update timer.java

--- timer.java
+++ timer.java
@@ -1,12 +1,15 @@
+import java.awt.Desktop;
+import java.net.URI;
 import java.util.Scanner;
 
 public class timer {
     public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Timer:Made By Soumya <3");
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter minutes: ");
-        int minutes = scanner.nextInt();
+        int minutes = sc.nextInt();
         System.out.print("Enter seconds: ");
-        int seconds = scanner.nextInt();
+        int seconds = sc.nextInt();
         int totalTimeInSeconds = (minutes * 60) + seconds;
 
         for (int i = totalTimeInSeconds; i >= 0; i--) {
@@ -17,5 +20,18 @@ public class timer {
         }
 
         System.out.println("\nTime's up!");
+        System.out.println("Do You Want To See More Project Like This 1-YES 2-No");
+        int choice = sc.nextInt();
+        if(choice == 1){
+            try {
+                URI link = new URI("https://github.com/biswalsoumya342");
+                Desktop.getDesktop().browse(link);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println("Thank You For Using This: For More Visit-[https://github.com/biswalsoumya342]");
+        }
     }
 }
